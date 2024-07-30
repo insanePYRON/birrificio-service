10e652e43107b6201a0f95cdc1d2de35d059b2f1
Alessandro Piron <a.piron@reply.it>
2024-07-30 17:52:23 +0200

    Initial commit Closes #1

---
+++ birrificio-service/src/main/java/insanepyron/birrificioservice/BirrificioServiceApplication.java
@@ -0,0 +1,13 @@
+package insanepyron.birrificioservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirrificioServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BirrificioServiceApplication.class, args);
+    }
+
+}
---
+++ birrificio-service/src/test/java/insanepyron/birrificioservice/BirrificioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package insanepyron.birrificioservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirrificioServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
